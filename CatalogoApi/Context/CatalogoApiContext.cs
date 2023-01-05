4f42d03e1ff0a495ac4cb2a3e405a60344eea789
gilbertosantana <gilbertosantoss307@gmail.com>
2023-01-05 19:54:12 -0300

    EF Core configurado e adicionada classe de contexto

---
+++ CatalogoApi/Context/CatalogoApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CatalogoApi.Models;
+namespace CatalogoApi.Context;
+
+public class CatalogoApiContext : DbContext
+{
+    public CatalogoApiContext(DbContextOptions<CatalogoApiContext> options)
+        : base(options) 
+    {
+
+    }
+
+    public DbSet<Produto>? Produtos { get; set; }
+    public DbSet<Categoria>? Categorias { get; set; }
+}
