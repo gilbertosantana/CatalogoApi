b334cd804ae55ffb99cddd23335ff8ab53bd7351
gilbertosantana <gilbertosantoss307@gmail.com>
2023-01-09 20:31:41 -0300

    Criando ProdutosController

---
+++ CatalogoApi/Controllers/ProdutosController.cs
@@ -0,0 +1,18 @@
+﻿using CatalogoApi.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        public readonly CatalogoApiContext _context;
+
+        public ProdutosController(CatalogoApiContext context)
+        {
+            _context = context;
+        }
+    }
+}
