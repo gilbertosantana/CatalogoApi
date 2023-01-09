1fbf8ef39439c0c524061397f41fa2e7fe3d5e3c
gilbertosantana <gilbertosantoss307@gmail.com>
2023-01-09 18:15:04 -0300

    Populando a tabela Categorias utilizando migrations

---
+++ CatalogoApi/Migrations/20230109210555_PopulaCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogoApi.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO CATEGORIAS(Nome, ImagemUrl) VALUES('Bebidas', 'bebidas.jpg')");
+            mb.Sql("INSERT INTO CATEGORIAS(Nome, ImagemUrl) VALUES('Lanches', 'lanches.jpg')");
+            mb.Sql("INSERT INTO CATEGORIAS(Nome, ImagemUrl) VALUES('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM CATEGORIAS");
+        }
+    }
+}
