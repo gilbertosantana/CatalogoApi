3742816566bb257dbc2456c43800e147d5b23b96
gilbertosantana <gilbertosantoss307@gmail.com>
2023-01-10 00:03:39 -0300

    CRUD completo de produtos

--- CatalogoApi/Controllers/ProdutosController.cs
+++ CatalogoApi/Controllers/ProdutosController.cs
@@ -27,7 +27,7 @@ namespace CatalogoApi.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="ObterProduto")]
         public ActionResult<Produto> Get(int? id) 
         {
             var produto = _context.Produtos?.FirstOrDefault(x => x.ProdutoId == id);
@@ -38,6 +38,45 @@ namespace CatalogoApi.Controllers
             }
 
             return produto;
+        }
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if(produto is null)
+            {
+                return BadRequest();
+            }
+            _context.Produtos?.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId, produto});
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if(id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos?.FirstOrDefault(x => x.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound("Produto não localizado...");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
     }
 }
