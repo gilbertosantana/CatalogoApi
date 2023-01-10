132fe370c1c44338509892a60ff84eded61913eb
gilbertosantana <gilbertosantoss307@gmail.com>
2023-01-09 22:36:29 -0300

    Adicionado Get para retornar todos os produtos e Get para retornar produto por id

--- CatalogoApi/Controllers/ProdutosController.cs
+++ CatalogoApi/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using CatalogoApi.Context;
+using CatalogoApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,30 @@ namespace CatalogoApi.Controllers
         public ProdutosController(CatalogoApiContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos?.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados...");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int? id) 
+        {
+            var produto = _context.Produtos?.FirstOrDefault(x => x.ProdutoId == id);
+
+            if(produto == null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+
+            return produto;
         }
     }
 }
